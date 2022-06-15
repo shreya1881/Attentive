623cd8739d2a203e5c90f039ef98b3be3a94864b
shreya1881 <2704shreya@gmail.com>
2022-06-15 09:34:21 +0530

    Login page - 7/6/22 to 10/6/22 Login Page linked to Sign In Page (login_page.dart) SignIn Page for teacher completed (Linking Login button remaining) - 11/6/2022

--- lib/splash.dart
+++ lib/splash.dart
@@ -19,7 +19,7 @@ class _SplashState extends State<Splash> {
   }
 
   _navigatetohome() async {
-    await Future.delayed(Duration(seconds: 5), () {});
+    await Future.delayed(Duration(seconds: 3), () {});
     Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => const SignUp()));
   }
@@ -27,27 +27,62 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xfff665BE9),
+      backgroundColor: Color(0xFFF4E4AC),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-                height: 100,
-                width: 100,
-                child: Image.asset(
-                  'images/Booklogo.png',
-                  height: 40.0,
-                  width: 30.0,
-                )),
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Color(0xFFF1CBF8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.8),
+                    spreadRadius: 7,
+                    blurRadius: 5,
+                    offset: Offset(0, 5), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Stack(
+                children: [
+                  Center(
+                    child: Image.asset(
+                      'images/Booklogo.png',
+                      height: 400.0,
+                      width: 300.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                    child: Text(
+                      'A',
+                      style: TextStyle(
+                        fontFamily: 'Bayon',
+                        fontSize: 70,
+                        color: Color(0xFFF4E4AC),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
             Text(
               'ATTENTIVE',
               style: TextStyle(
                   fontSize: 35.0,
                   fontWeight: FontWeight.bold,
-                  letterSpacing: 5.0,
+                  letterSpacing: 3.0,
                   fontFamily: 'Comfortaa',
-                  color: Colors.white),
+                  color: Colors.black),
             )
           ],
         ),
